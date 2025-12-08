57f5c46fe513d3ee7ab1342aaed1116fb071fb42
nikolaidiphmm-source <nikol@MacBookAir.home>
2025-12-08 11:34:51 +0200

    fix: add new prints

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -3,5 +3,7 @@ package gr.aueb.cf.testgit;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Coding!");
+        System.out.println("Hello Coding Factory!!!!!");
     }
 }
